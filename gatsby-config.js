2346617483832b313069373da22e5833e720cfdd
bjhcoutts@gmail.com <bjhcoutts@gmail.com>
2019-07-09 09:48:20 -0700

    completed documentation examples

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,16 @@
 module.exports = {
+  siteMetadata: {
+    title: `PANDA PANDA PANDA eating all de tings`,
+  },
   plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `src`,
+        path: `${__dirname}/src/`,
+      },
+    },
+    `gatsby-transformer-remark`,
     `gatsby-plugin-emotion`,
     {
       resolve: `gatsby-plugin-typography`,
